98a9677615d1f90068d274a5c4cf126828690c4f
Thabang Matlou <thabangmatlou29@gmail.com>
2026-06-04 17:30:06 +0200

    Update JS validation

--- validation.js
+++ validation.js
@@ -2,6 +2,9 @@ document.getElementById('signUpForm').addEventListener('submit', function(event)
 
     event.preventDefault();
 
+    let isValid = true
+
+    //get elements by Ids
     const username = document.getElementById('username');
     const email = document.getElementById('email');
     const password = document.getElementById('password');
@@ -11,6 +14,35 @@ document.getElementById('signUpForm').addEventListener('submit', function(event)
     const emailError = document.getElementById('emailError');
     const passwordError = document.getElementById('passwordError');
     const checkboxError = document.getElementById('checkboxError');
+
+// Perfom Validations
+
+//username
+if(username.value.trim()=== ''){
+    usernameError.textContent = 'Username is required';
+    username.style.borderColor = 'red';
+    isValid = false;
+}
+
+//email
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+if(email.value.trim()=== ''){
+    emailError.textContent = 'Email address is required'
+    email.style.borderColor = 'red'
+    isValid = false;
+}else if (!emailRegex.test(email.value.trim())){
+    emailError.textContent = 'Please enter a valid email address'
+    inValid = false
+}
+
+//password
+
+if(password.valu.trim() === ''){
+    passwordError.textContent ='Password is required';
+    isValid = false
+}
 
 })
 
