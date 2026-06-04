05db463e1c07084bff43458dbdf023d88f980bd1
Thabang Matlou <thabangmatlou29@gmail.com>
2026-06-04 12:30:16 +0200

    Signup  and Validation update

---
+++ validation.js
@@ -0,0 +1,16 @@
+document.getElementById('signUpForm').addEventListener('submit', function(event){
+
+    event.preventDefault();
+
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const checkbox = document.getElementById('checkbox');
+
+    const usernameError = document.getElementById('usernameError');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+    const checkboxError = document.getElementById('checkboxError');
+
+})
+
