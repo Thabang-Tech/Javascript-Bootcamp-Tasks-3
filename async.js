15dfbb7282e93a036543916ccf75888a847f5197
Thabang Matlou <thabangmatlou29@gmail.com>
2026-06-06 03:06:21 +0200

    Fake database

--- async.js
+++ async.js
@@ -2,46 +2,15 @@
 
 const studentDatabase = {
     profiles: [
-        {
-            id: 1998,
-            firstName: 'Mahlatse',
-            lastName: 'Sarithi',
-            email: 'mahlatse@gmail.com',
-            course: 'Java',
-            age: 28
-        },
-        {
-            id: 1999,
-            firstName: 'Frankie',
-            lastName: 'SMosehla',
-            email: 'frankie@gmail.com',
-            course: 'Python',
-            age: 27
-        },
-        {
-            id: 1997,
-            firstName: 'Sabelo',
-            lastName: 'Gumede',
-            email: 'sabelo@gmail.com',
-            course: 'Html',
-            age: 29
-        },
-        {
-            id: 2000,
-            firstName: 'Thabang',
-            lastName: 'Matlou',
-            email: 'thabang@gmail.com',
-            course: 'CSS',
-            age: 25
-        },
-        {
-            id: 1995,
-            firstName: 'Dlozi',
-            lastName: 'Sarithi',
-            email: 'dlozi@gmail.com',
-            course: 'CPP',
-            age: 27
-        }
+        { id: 1998, firstName: 'Mahlatse', lastName: 'Sarithi',email: 'mahlatse@gmail.com', course: 'Java', age: 28},
+
+        { id: 1999, firstName: 'Frankie', lastName: 'SMosehla', email: 'frankie@gmail.com', course: 'Python', age: 27},
+
+        { id: 1997, firstName: 'Sabelo', lastName: 'Gumede', email: 'sabelo@gmail.com', course: 'Html', age: 29},
+
+        { id: 2000, firstName: 'Thabang', lastName: 'Matlou', email: 'thabang@gmail.com', course: 'CSS', age: 25},
+
+        { id: 1995, firstName: 'Dlozi', lastName: 'Mthethwa', email: 'dlozi@gmail.com', course: 'CPP',age: 27}
     ] 
 
 }
